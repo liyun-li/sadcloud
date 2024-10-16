6a5277b3a5ba4e62ffa6a4211276cad7f5d2fa19
Liyun <7397775-liyun-li@users.noreply.gitlab.com>
2024-10-16 14:43:14 -0400

    add 2.7.6

--- gcloud/modules/kms/main.tf
+++ gcloud/modules/kms/main.tf
@@ -8,3 +8,10 @@ resource "google_kms_crypto_key" "insecure_crypto_key" {
   key_ring        = google_kms_key_ring.insecure_keyring.id
   rotation_period = "630720000s" # 20 years
 }
+
+# 2.7.6	Ensure That Cloud KMS Cryptokeys Are Not Anonymously or Publicly Accessible
+resource "google_kms_crypto_key_iam_member" "all_users" {
+  crypto_key_id = google_kms_crypto_key.insecure_crypto_key.id
+  role          = "roles/cloudkms.admin"
+  member        = "allUsers"
+}
