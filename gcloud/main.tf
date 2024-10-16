036da8cc398adac19e777d3a54c10eefddaa0a8b
Liyun <7397775-liyun-li@users.noreply.gitlab.com>
2024-10-16 12:11:01 -0400

    quick save

--- gcloud/main.tf
+++ gcloud/main.tf
@@ -1,32 +1,32 @@
-# module "compute" {
-#   source = "./modules/compute"
-# }
+module "compute" {
+  source = "./modules/compute"
+}
 
-# module "database" {
-#   source = "./modules/database"
-# }
+module "database" {
+  source = "./modules/database"
+}
 
-# module "functions" {
-#   source   = "./modules/functions"
-#   location = var.region
-# }
+module "functions" {
+  source   = "./modules/functions"
+  location = var.region
+}
 
-# module "storage" {
-#   source = "./modules/storage"
-# }
+module "storage" {
+  source = "./modules/storage"
+}
 
-# module "kms" {
-#   source = "./modules/kms"
-# }
+module "kms" {
+  source = "./modules/kms"
+}
 
-# module "iam" {
-#   source  = "./modules/iam"
-#   project = var.project
-# }
+module "iam" {
+  source  = "./modules/iam"
+  project = var.project
+}
 
-# module "networking" {
-#   source = "./modules/networking"
-# }
+module "networking" {
+  source = "./modules/networking"
+}
 
 module "dns" {
   source = "./modules/dns"
